c1bf0d17647d840fff789ea03d3bafb760246ac8
aslanariq237 <aslanariq009@gmail.com>
2023-05-10 10:43:18 +0700

    kedua

---
+++ src/views/page/dashboard/Project/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Project from './project'
+
+function Index() {
+    return(
+        <div className="">
+            <Project />
+        </div>
+    )
+}
+export default Index
